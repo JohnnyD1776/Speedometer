864fde6fab7f0a3ddd61f8eed4465ceac91191a4
John Durcan <jdurcan@oanda.com>
2025-06-03 18:27:22 +0100

    Initial Commit

---
+++ Speedometer/SpeedometerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeedometerApp.swift
+//  Speedometer
+//
+//  Created by John Durcan on 03/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct SpeedometerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
